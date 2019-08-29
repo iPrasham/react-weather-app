7988e296662f6adb3315a7e93945777f7d676362
Prasham Jain <prshmjain1@gmail.com>
2019-08-30 02:43:32 +0530

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  render(){
+    return(
+      <Fragment>
+        <Header />
+        <br />
+        <div className="container">
+          <input className="searchBox" type="text" placeholder="Enter City Name..." />
+          <button className="searchButton" type="submit">Search</button>
+        </div>
+      </Fragment>
+  )}
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = function(){
+    return <div>
+        <h3 className="header">Weather App</h3>
+    </div>
+};
+
+export default Header;
